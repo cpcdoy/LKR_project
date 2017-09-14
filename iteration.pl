d27f25d0554b05ca40d66f8042d11272e62b4781
Chady Dimachkie <cpcdoy@gmail.com>
2017-09-14 21:20:02 +0200

    Add: symmetry string output

--- iteration.pl
+++ iteration.pl
@@ -19,6 +19,10 @@ get_suffix_array(L, R) :-
     get_suffix_array_hat(N, L, Tmp),
     reverse(Tmp, R).
 
+get_prefix_array(L, R) :-
+    reverse(L, Tmp),
+    get_suffix_array(Tmp, R).
+
 get_suffix_array_hat(N, _, R) :-
     N == -1,
     R = [],
@@ -28,90 +32,80 @@ get_suffix_array_hat(N, L, [Elt|R]) :-
     I is N - 1,
     get_suffix_array_hat(I, L, R).
 
-% Pass L as suffix array
-compare_all_suffixes(L, Cnt, R) :-
-    get_suffix_array(L, SuffArray),
-    compare_all_suffixes_hat(0, SuffArray, 0, [], Cnt, R).
-    
-compare_all_suffixes_hat(N, L, Cnt, LongestSuff, Hcnt, R) :-
-    length(L, Size),
-    I is Size - N,
-    I == 0,
-    Hcnt = Cnt,
-    R = LongestSuff,
-    !.
-
-compare_all_suffixes_hat(N, L, Cnt, StoredSuff, Hcnt, R) :-
-    nth0(N , L, NewSuff),
-    nth0(N , L, NewSuffTmp),
-    compare_suffix(NewSuff, NewSuffTmp, N, 0, L, Nb, CurSuff),
-    compare_repeated_suffixes(StoredSuff, CurSuff, Nb, Cnt, CntRes, SuffRes),
-    I is N + 1,
-    compare_all_suffixes_hat(I, L, CntRes, SuffRes, Hcnt, R).
-
-compare_repeated_suffixes(_, CurSuff, Nb, Cnt, CntRes, R) :-
-    Nb > Cnt,
-   	R = CurSuff,
-    CntRes = Nb,
-    !.
-compare_repeated_suffixes(StoredSuff, _, Nb, Cnt, CntRes, R) :-
-    Nb < Cnt,
-   	R = StoredSuff,
-    CntRes = Cnt,
-    !.
-compare_repeated_suffixes(StoredSuff, CurSuff, Nb, Cnt, CntRes, R) :-
-    Nb == Cnt,
-    length(CurSuff, CurSuffLen),
-    length(StoredSuff, StoredSuffLen),
-    CurSuffLen > StoredSuffLen,
-    R = CurSuff,
-    CntRes = Nb,
-    !.
-compare_repeated_suffixes(StoredSuff, _, Nb, Cnt, CntRes, R) :-
-    Nb == Cnt,
-    R = StoredSuff,
-    CntRes = Nb,
-    !.
-    
-compare_all_suffixes_hat(N, L, Cnt, LongestSuff, Hcnt, R) :-
-    I is N + 1,
-    compare_all_suffixes_hat(I, L, Cnt, LongestSuff, Hcnt, R).
-
-compare_suffix(Sub, SubTmp, _, Cnt, _, Hcnt, R) :-
-    length(SubTmp, Size),
-    Size == 0,
-    Hcnt = Cnt,
-    R = Sub,
-    !.
-
-compare_suffix(_, SubTmp, SubIdx, Cnt, L, Hcnt, R) :-
-    compare_suffix_hat(0, 0, SubTmp, SubIdx, L, NewCnt),
-    NewCnt > Cnt,
-    append(NewSub, [_], SubTmp),
-    compare_suffix(SubTmp, NewSub, SubIdx, NewCnt, L, Hcnt, R).
-
-compare_suffix(Sub, SubTmp, SubIdx, Cnt, L, Hcnt, R) :-
-    append(NewSub, [_], SubTmp),
-    compare_suffix(Sub, NewSub, SubIdx, Cnt, L, Hcnt, R).
-
-compare_suffix_hat(N, Cnt, _, _, L, R) :-
+compare_suffix(Sub, L, R) :-
+    compare_suffix_hat(0, 0, Sub, L, R).
+compare_suffix_hat(N, Cnt, _, L, R) :-
     length(L, Size),
     Size == N,
     R = Cnt,
     !.
-
-compare_suffix_hat(N, Cnt, Sub, SubIdx, L, R) :-
+compare_suffix_hat(N, Cnt, Sub, L, R) :-
     nth0(N, L, Suff),
     prefix(Sub, Suff),
-    length(Sub, SubLen),
-    is_continuous(SubLen, SubIdx, N, Sub, Suff),
     I is N + 1,
     Incr is Cnt + 1,
-    compare_suffix_hat(I, Incr, Sub, SubIdx, L, R).
-
-compare_suffix_hat(N, Cnt, Sub, SubIdx, L, R) :-
+    compare_suffix_hat(I, Incr, Sub, L, R).
+compare_suffix_hat(N, Cnt, Sub, L, R) :-
     I is N + 1,
-    compare_suffix_hat(I, Cnt, Sub, SubIdx, L, R).
+    compare_suffix_hat(I, Cnt, Sub, L, R).
+
+prefix(X, L) :-
+    append(X, _, L).
+suffix(X, L) :-
+    append(_, X, L).
+sublist(X, L) :-
+    suffix(S, L),
+    prefix(X, S),
+    length(X, R),
+    R > 0.
+
+find_elem_in_list(X, [X|_], X).
+find_elem_in_list(X, [_|L], _) :-
+    find_elem_in_list(X, L, _).
+
+match_suffix_prefix([X|_], [X|_], X).
+match_suffix_prefix([X|_], L2, X) :-
+    find_elem_in_list(X, L2, X),
+    !.
+
+match_suffix_prefix([_|L1], L2, X) :-
+    match_suffix_prefix(L1, L2, X).
+
+%
+label_start(Max, _, Max, _, Off, Off).
+label_start(Max, _, Max, _, Off, _) :-
+    label_start(Max, _, Max, _, Off, Off).
+
+label_start(Max, Len, G, I, Off, R) :-
+    G < Max,
+    G1 is G + 1,
+    I1 is I + 1,
+    I1 == Len,
+    Off1 is Off + 1,
+    Len1 is Len - 1,
+    label_start(Max, Len1, G1, 0, Off1, R).
+
+label_start(Max, Len, G, I, Off, R) :-
+    G < Max,
+    I1 is I + 1,
+    G1 is G + 1,
+    label_start(Max, Len, G1, I1, Off, R).
+
+:- dynamic a/0. 
+get_all_start_labels(L, LenS, LenL, R, S) :-
+   not(a),
+   assert(a),
+   R = [],
+   get_all_start_labels(L, LenS, LenL, R, S).
+get_all_start_labels([], _, _, R, R) :-
+   retract(a),
+   !.
+get_all_start_labels([], _, _, R, _) :-
+    get_all_start_labels(_, _, _, R, R).
+get_all_start_labels([_|L], I, LenL, R, S) :-
+    I1 is I + 1,
+    label_start(I1, LenL, 0, 0, 0, Label),
+    get_all_start_labels(L, I1, LenL, [Label|R], S).
 
 take(0, _, []).
 take(N, [H|T], [H|R]) :-
@@ -125,53 +119,64 @@ drop(N, [_|T], R) :-
     M is N - 1,
     drop(M, T, R).
 
-is_continuous(_, SubIdx, SuffIdx, _, _) :-
-    SubIdx == SuffIdx,
-    !.
-is_continuous(SubLen, SubIdx, SuffIdx, Sub, Suff) :-
-    drop(SubLen, Suff, NewSuff),
-    prefix(Sub, NewSuff),
-    NewSuffIdx is SuffIdx + SubLen,
-    is_continuous(SubLen, SubIdx, NewSuffIdx, Sub, NewSuff).
-
-iteration(L, R) :-
-    compare_all_suffixes(L, Cnt, RepeatedSq),
-    cut_string(L, [], Cnt, RepeatedSq, Rest, R).
-
-cut_string(L, Acc, Cnt, RepeatedSq, Rest, R) :-
+cut_string(L, Acc, RepeatedSq, Rest, R) :-
     prefix(RepeatedSq, L),
     Acc == [],
     length(RepeatedSq, Len),
-    LenToCut is Len * Cnt,
+    LenToCut is Len,
     drop(LenToCut, L, Tmp),
-    append([Cnt, "*", "("|RepeatedSq], [")", "+"|Tmp], Res),
+    append([ "S", "("|RepeatedSq], [")", "+"|Tmp], Res),
     R = Res,
     !.
-cut_string(L, Acc, Cnt, RepeatedSq, Rest, R) :-
+cut_string(L, Acc, RepeatedSq, Rest, R) :-
     prefix(RepeatedSq, L),
     Rest = Acc,
     length(RepeatedSq, Len),
-    LenToCut is Len * Cnt,
+    LenToCut is Len,
     length(L, ExactLen),
     LenToCut == ExactLen,
-    append(Rest, ["+", Cnt , "*", "("| RepeatedSq], Tmp),
+    append(Rest, ["+" , "S", "("| RepeatedSq], Tmp),
     append(Tmp, [")"], Rtmp),
     R = Rtmp,
     !.
-cut_string(L, Acc, Cnt, RepeatedSq, Rest, R) :-
+cut_string(L, Acc, RepeatedSq, Rest, R) :-
     prefix(RepeatedSq, L),
     Rest = Acc,
     length(RepeatedSq, Len),
-    LenToCut is Len * Cnt,
+    LenToCut is Len,
     drop(LenToCut, L, Tmp),
-    append(Rest, ["+", Cnt , "*", "("| RepeatedSq], LeftPart),
+    append(Rest, ["+" , "S", "("| RepeatedSq], LeftPart),
     append(LeftPart, [")"], Rtmp),
     append(Rtmp, ["+"|Tmp], Res),
     R = Res,
     !.
-cut_string(L, Acc, Cnt, RepeatedSq, Rest, R) :-
+cut_string(L, Acc, RepeatedSq, Rest, R) :-
     take(1, L, Elt),
     append(Elt, Acc, NewAcc),
 	drop(1, L, DroppedL),
-    cut_string(DroppedL, NewAcc, Cnt, RepeatedSq, Rest, R).
-%iteration([a,a,a,a,a,y,o,y,o,a], R).
+    cut_string(DroppedL, NewAcc, RepeatedSq, Rest, R).
+
+get_symmetry(L, S, R) :-
+    get_symmetry_(L, S),
+    %findall(X, get_symmetry_(L, X), S).
+    cut_string(L, [], S, Rest, R).
+
+get_symmetry_(L, X) :-
+    findall(Sub, sublist(Sub, L), S),
+  	member(Sub, S),
+    get_symmetry_lists([Sub], X).
+
+get_symmetry_lists([L|_], X) :-
+    get_symmetry_list(L, X),
+    !.
+
+get_symmetry_lists([_|S], X) :-
+	get_symmetry_lists(S, X).
+
+get_symmetry_list(L, X) :-
+    get_suffix_array(L, R1),
+    get_prefix_array(L, R2),
+    match_suffix_prefix(R1, R2, X),
+    length(X, R),
+    R > 2,
+    not(0 is (R mod 2)).
