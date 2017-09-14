2ad64810db8f713600829d372df25167280361f9
Axel Mendoza <axel.mendoza@epita.fr>
2017-09-14 18:34:18 +0200

    almost finish iteration

--- iteration.pl
+++ iteration.pl
@@ -43,33 +43,47 @@ compare_all_suffixes_hat(N, L, Cnt, LongestSuff, Hcnt, R) :-
 
 compare_all_suffixes_hat(N, L, Cnt, _, Hcnt, R) :-
     nth0(N , L, NewSuff),
-    compare_suffix(NewSuff, N, L, Nb),
+    nth0(N , L, NewSuffTmp),
+    compare_suffix(NewSuff, NewSuffTmp, N, 0, L, Nb, SuffRes),
     Nb > Cnt,
     I is N + 1,
-    compare_all_suffixes_hat(I, L, Nb, NewSuff, Hcnt, R).
+    compare_all_suffixes_hat(I, L, Nb, SuffRes, Hcnt, R).
+
 compare_all_suffixes_hat(N, L, Cnt, LongestSuff, Hcnt, R) :-
     I is N + 1,
     compare_all_suffixes_hat(I, L, Cnt, LongestSuff, Hcnt, R).
 
-compare_suffix(Sub, SubIdx, L, R) :-
-    compare_suffix_hat(0, 0, Sub, SubIdx, L, R).
+compare_suffix(Sub, SubTmp, _, Cnt, _, Hcnt, R) :-
+    length(SubTmp, Size),
+    Size == 0,
+    Hcnt = Cnt,
+    R = Sub,
+    !.
+
+compare_suffix(_, SubTmp, SubIdx, Cnt, L, Hcnt, R) :-
+    compare_suffix_hat(0, 0, SubTmp, SubIdx, L, NewCnt),
+    NewCnt > Cnt,
+    append(NewSub, [_], SubTmp),
+    compare_suffix(SubTmp, NewSub, SubIdx, NewCnt, L, Hcnt, R).
+compare_suffix(Sub, SubTmp, SubIdx, Cnt, L, Hcnt, R) :-
+    append(NewSub, [_], SubTmp),
+    compare_suffix(Sub, NewSub, SubIdx, Cnt, L, Hcnt, R).
+
 compare_suffix_hat(N, Cnt, _, _, L, R) :-
     length(L, Size),
     Size == N,
     R = Cnt,
     !.
+
 compare_suffix_hat(N, Cnt, Sub, SubIdx, L, R) :-
     nth0(N, L, Suff),
     prefix(Sub, Suff),
     length(Sub, SubLen),
-    length(Suff, SuffLen),
-    Idx is SubIdx - N,
-    Dist is SuffLen - SubLen,
-    Idx == Dist,
     is_continuous(SubLen, SubIdx, N, Sub, Suff),
     I is N + 1,
     Incr is Cnt + 1,
     compare_suffix_hat(I, Incr, Sub, SubIdx, L, R).
+
 compare_suffix_hat(N, Cnt, Sub, SubIdx, L, R) :-
     I is N + 1,
     compare_suffix_hat(I, Cnt, Sub, SubIdx, L, R).
@@ -81,7 +95,7 @@ take(N, [H|T], [H|R]) :-
     take(M, T, R).
 
 drop(0, L, L).
-drop(N, [H|T], R) :-
+drop(N, [_|T], R) :-
     N > 0,
     M is N - 1,
     drop(M, T, R).
@@ -94,3 +108,5 @@ is_continuous(SubLen, SubIdx, SuffIdx, Sub, Suff) :-
     prefix(Sub, NewSuff),
     NewSuffIdx is SuffIdx + SubLen,
     is_continuous(SubLen, SubIdx, NewSuffIdx, Sub, NewSuff).
+
+%compare_all_suffixes([c,c,o,w,c,o,w,w], Cnt, R).
